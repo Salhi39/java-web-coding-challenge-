e44912f0444c86fd7e38370eaeccbdc4a500009b
MedSALHI <m.salhi@dialy.net>
2018-01-14 02:02:24 +0000

    add register component

--- JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/register/register.component.ts
+++ JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../server/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService
+  ) {
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.register(this.form.value);
+    }
+    this.formSubmitAttempt = true;
   }
 
 }
