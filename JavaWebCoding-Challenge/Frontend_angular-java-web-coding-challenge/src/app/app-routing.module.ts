533d1f3cc147ea8e772aa82896927d5bc6db27fe
MedSALHI <m.salhi@dialy.net>
2018-01-14 02:01:42 +0000

    config route for register component

--- JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/app-routing.module.ts
+++ JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {LoginComponent} from './front/login/login.component';
 import {AuthGuard} from './server/auth/auth.guard';
 import {HomeComponent} from './front/home/home.component';
 import {ShopComponent} from './front/shop/shop.component';
+import {RegisterComponent} from './front/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
