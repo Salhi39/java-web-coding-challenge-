8ac8abb3113868e6b1f518df4fe14e72b4e7ef31
MedSALHI <m.salhi@dialy.net>
2018-01-17 01:54:20 +0000

    fix bug when one item clicked more than twice time

--- JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/header/header.component.ts
+++ JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/header/header.component.ts
@@ -1,36 +1,60 @@
-import { Observable } from 'rxjs/Observable';
-import { AuthService } from '../../server/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
-import { MatToolbarModule } from '@angular/material';
+import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../../server/auth/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Security} from '../../server/security';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-    `.angular-logo {
-        margin: 0 4px 3px 0;
-        height: 35px;
-        vertical-align: middle;
-    }
-    .fill-remaining-space {
-      flex: 1 1 auto;
-    }
-    `
-  ]
+  styleUrls: ['./header.component.css']
 })
 
 
 export class HeaderComponent implements OnInit {
 
   isLoggedIn$: Observable<boolean>;
+  isFavoriteEnabled: boolean;
+  isListEnabled: boolean;
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit() {
-    this.isLoggedIn$ = this.authService.isLoggedIn;
+  constructor(private router: Router, private authService: AuthService) {
+    this.isFavoriteEnabled = false;
+    this.isListEnabled = true;
   }
 
-  onLogout(){
+  ngOnInit() {
+    this.isFavoriteEnabled = false;
+    this.isListEnabled = true;
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+    this.isLoggedIn$.subscribe(value => {
+      if (!value) {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  onLogout() {
+    this.isFavoriteEnabled = false;
+    this.isListEnabled = false;
     this.authService.logout();
+  }
+
+
+  goToPreferred() {
+    if (this.isListEnabled) {
+      const userEmail = this.authService.userEmail;
+      this.router.navigate(['/my_favorite'], {queryParams: {id: Security.encrypt(userEmail)}});
+      this.isFavoriteEnabled = true;
+      this.isListEnabled = false;
+    }
+  }
+
+  goToAllShop() {
+    if (this.isFavoriteEnabled) {
+      const userEmail = this.authService.userEmail;
+      this.router.navigate(['/'], {queryParams: {id: Security.encrypt(userEmail)}});
+      this.isListEnabled = true;
+      this.isFavoriteEnabled = false;
+    }
   }
 }
