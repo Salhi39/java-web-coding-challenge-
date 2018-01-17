bb414dbc62783f39b8edbeeba7a10738fb63f280
MedSALHI <m.salhi@dialy.net>
2018-01-17 01:57:58 +0000

    fix bug

--- JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/shop/shop.component.ts
+++ JavaWebCoding-Challenge/Frontend_angular-java-web-coding-challenge/src/app/front/shop/shop.component.ts
@@ -21,77 +21,78 @@ import CryptoJS = require('crypto-js');
 export class ShopComponent implements OnInit {
 
   // shop object
-  @Input() private shop: Shops;
+  @Input() private _shop: Shops;
   // shops that will be shown ( depend of paginator )
-  private shopList: Shops[];
+  private _shopList: Shops[];
   // all shops
-  private constShopList: Shops[];
+  private _constShopList: Shops[];
 
   // Number of column on the page
-  private nbCols: number;
+  private _nbCols: number;
 
   // if icon is on (true) or off (false)
-  private like: boolean[];
-  private dislike: boolean[];
+  private _like: boolean[];
+  private _dislike: boolean[];
 
-  private userEmail: string;
+  private _userEmail: string;
 
   // MatPaginator Output
-  pageEvent: PageEvent;
+  private _pageEvent: PageEvent;
 
   //  View city dialog
-  detailtsDialogRef: MatDialogRef<DetailsDialogComponent>;
+  private _detailsDialogRef: MatDialogRef<DetailsDialogComponent>;
 
 
   // MatPaginator Inputs
-  private length = 100;
-  private pageSize = 10;
-  private pageSizeOptions = [5, 10, 25, 100];
+  private _length = 100;
+  private _pageSize = 10;
+  private _pageSizeOptions = [5, 10, 25, 100];
 
-  // Paginator intetrval
-  private from = 0;
-  private to = 10;
+  // Pagination Interval
+  private _from = 0;
+  private _to = 10;
 
 
-  constructor(private router: ActivatedRoute, private apiService: ApiService, private dialog: MatDialog) {
-    this.shopList = [];
-    this.constShopList = [];
+  constructor(private _router: ActivatedRoute, private _apiService: ApiService, private _dialog: MatDialog) {
+    this._shopList = [];
+    this._constShopList = [];
   }
 
 
   ngOnInit() {
     // init grid column
-    this.nbCols = this.adaptColumn(window.screen.width);
-    this.like = [];
-    this.dislike = [];
+    this._nbCols = this.adaptColumn(window.screen.width);
+    this._like = [];
+    this._dislike = [];
     // get all shops
-    this.router.queryParams.forEach(value => {
-      this.apiService.getAllNotCommentedShops(Security.decrypt(value['id'])).subscribe(
+    this._router.queryParams.forEach(value => {
+      console.log('-------- ' + Security.decrypt(value['id']));
+      this._apiService.getAllNotCommentedShops(Security.decrypt(value['id'])).subscribe(
         val => {
-          this.length = val.length;
+          this._length = val.length;
 
           for (const elm of val) {
-            this.shop = new Shops();
-            this.shop.setId(elm['_id']);
-            this.shop.setCity(elm['city']);
-            this.shop.setEmail(elm['email']);
-            this.shop.setPicture(elm['picture']);
-            this.shop.setLocation(elm['location']);
-            this.shop.setName(elm['name']);
-            this.constShopList.push(this.shop);
+            this._shop = new Shops();
+            this._shop.setId(elm['_id']);
+            this._shop.setCity(elm['city']);
+            this._shop.setEmail(elm['email']);
+            this._shop.setPicture(elm['picture']);
+            this._shop.setLocation(elm['location']);
+            this._shop.setName(elm['name']);
+            this._constShopList.push(this._shop);
 
-            this.like.push(false);
-            this.dislike.push(false);
+            this._like.push(false);
+            this._dislike.push(false);
           }
           // in the first time only 10 shops will be display
           let max = 10;
           // if there is less than 10 shops in my database
           // show me what I have
-          if (this.constShopList.length < 10) {
-            max = this.constShopList.length;
+          if (this._constShopList.length < 10) {
+            max = this._constShopList.length;
           }
           // 10 items
-          this.shopList = this.constShopList.slice(0, max);
+          this._shopList = this._constShopList.slice(0, max);
         });
     });
   }
@@ -99,7 +100,7 @@ export class ShopComponent implements OnInit {
 
   // open Dialog by given location
   openDialog(location) {
-    this.detailtsDialogRef = this.dialog.open(DetailsDialogComponent, {
+    this._detailsDialogRef = this._dialog.open(DetailsDialogComponent, {
       width: '100%',
       height: '90%',
       data: {'location': location},
@@ -115,23 +116,23 @@ export class ShopComponent implements OnInit {
   updatePagination() {
 
     // to avoid the overflowing of the page length
-    const lastValue = this.to;
+    const lastValue = this._to;
 
     // calculate start and end of the new interval
-    this.from = this.pageEvent.pageSize * this.pageEvent.pageIndex;
-    this.to = this.pageEvent.pageSize * (this.pageEvent.pageIndex + 1);
-    if (this.to > this.pageEvent.length) {
-      this.to = this.pageEvent.length;
+    this._from = this._pageEvent.pageSize * this._pageEvent.pageIndex;
+    this._to = this._pageEvent.pageSize * (this._pageEvent.pageIndex + 1);
+    if (this._to > this._pageEvent.length) {
+      this._to = this._pageEvent.length;
     }
 
     // update the display list
-    this.shopList = this.constShopList.slice(this.from, this.to);
+    this._shopList = this._constShopList.slice(this._from, this._to);
   }
 
   // update the column number to show per row
   onResize(event) {
     const element = event.target.innerWidth;
-    this.nbCols = this.adaptColumn(element);
+    this._nbCols = this.adaptColumn(element);
   }
 
 
@@ -144,38 +145,152 @@ export class ShopComponent implements OnInit {
   }
 
   onLike(index) {
-    this.dislike[index] = false;
-    this.like[index] = !this.like[index];
+    this._dislike[index] = false;
+    this._like[index] = !this._like[index];
 
-    this.router.queryParams.forEach(value => {
-      if (this.like[index]) {
+    this._router.queryParams.forEach(value => {
+      if (this._like[index]) {
         const userEmail = Security.decrypt(value['id']);
-        this.apiService.removeShopFromDisliked(userEmail, this.constShopList[index].getId());
-        this.apiService.addShopAsLiked(userEmail, this.constShopList[index].getId());
+        this._apiService.removeShopFromDisliked(userEmail, this._constShopList[index].getId());
+        this._apiService.addShopAsLiked(userEmail, this._constShopList[index].getId());
       }
       else {
         const userEmail = Security.decrypt(value['id']);
-        this.apiService.removeShopFromLiked(userEmail, this.constShopList[index].getId());
+        this._apiService.removeShopFromLiked(userEmail, this._constShopList[index].getId());
       }
 
     });
   }
 
   onDislike(index) {
-    this.like[index] = false;
-    this.dislike[index] = !this.dislike[index];
+    this._like[index] = false;
+    this._dislike[index] = !this._dislike[index];
 
-    this.router.queryParams.forEach(value => {
-      if (this.dislike[index]) {
-        this.userEmail = Security.decrypt(value['id']);
-        this.apiService.removeShopFromLiked(this.userEmail, this.constShopList[index].getId());
-        this.apiService.addShopAsDisliked(this.userEmail, this.constShopList[index].getId());
+    this._router.queryParams.forEach(value => {
+      if (this._dislike[index]) {
+        this._userEmail = Security.decrypt(value['id']);
+        this._apiService.removeShopFromLiked(this._userEmail, this._constShopList[index].getId());
+        this._apiService.addShopAsDisliked(this._userEmail, this._constShopList[index].getId());
       }
       else {
         const userEmail = Security.decrypt(value['id']);
-        this.apiService.removeShopFromDisliked(userEmail, this.constShopList[index].getId());
+        this._apiService.removeShopFromDisliked(userEmail, this._constShopList[index].getId());
       }
 
     });
+  }
+
+
+  get shop(): Shops {
+    return this._shop;
+  }
+
+  set shop(value: Shops) {
+    this._shop = value;
+  }
+
+  get shopList(): Shops[] {
+    return this._shopList;
+  }
+
+  set shopList(value: Shops[]) {
+    this._shopList = value;
+  }
+
+  get constShopList(): Shops[] {
+    return this._constShopList;
+  }
+
+  set constShopList(value: Shops[]) {
+    this._constShopList = value;
+  }
+
+  get nbCols(): number {
+    return this._nbCols;
+  }
+
+  set nbCols(value: number) {
+    this._nbCols = value;
+  }
+
+  get userEmail(): string {
+    return this._userEmail;
+  }
+
+  set userEmail(value: string) {
+    this._userEmail = value;
+  }
+
+  get pageEvent(): PageEvent {
+    return this._pageEvent;
+  }
+
+  set pageEvent(value: PageEvent) {
+    this._pageEvent = value;
+  }
+
+  get detailsDialogRef(): MatDialogRef<DetailsDialogComponent> {
+    return this._detailsDialogRef;
+  }
+
+  set detailsDialogRef(value: MatDialogRef<DetailsDialogComponent>) {
+    this._detailsDialogRef = value;
+  }
+
+  get length(): number {
+    return this._length;
+  }
+
+  set length(value: number) {
+    this._length = value;
+  }
+
+  get pageSize(): number {
+    return this._pageSize;
+  }
+
+  set pageSize(value: number) {
+    this._pageSize = value;
+  }
+
+  get pageSizeOptions(): number[] {
+    return this._pageSizeOptions;
+  }
+
+  set pageSizeOptions(value: number[]) {
+    this._pageSizeOptions = value;
+  }
+
+  get from(): number {
+    return this._from;
+  }
+
+  set from(value: number) {
+    this._from = value;
+  }
+
+  get to(): number {
+    return this._to;
+  }
+
+  set to(value: number) {
+    this._to = value;
+  }
+
+
+  get like(): boolean[] {
+    return this._like;
+  }
+
+  set like(value: boolean[]) {
+    this._like = value;
+  }
+
+  get dislike(): boolean[] {
+    return this._dislike;
+  }
+
+  set dislike(value: boolean[]) {
+    this._dislike = value;
   }
 }
